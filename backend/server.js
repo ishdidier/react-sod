15892cb5d64c1ee634cea9b2206e096a07ccfea4
ishdidier <134267242+ishdidier@users.noreply.github.com>
2025-05-16 16:31:17 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('✅ MongoDB connected'))
-.catch(err => console.error('❌ MongoDB error:', err));
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(' MongoDB error:', err));
 
 // Candidate Schema
 const candidateSchema = new mongoose.Schema({
